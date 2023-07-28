634ab80302719ab3af644aea86abbeabcea9b6fb
pahancha <pahanchathuranga123@gmail.com>
2023-07-29 03:42:15 +0530

    club model

---
+++ src/main/java/com/techiesmeetups/web/models/Club.java
@@ -0,0 +1,98 @@
+package com.techiesmeetups.web.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "clubs")
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String photoURL;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+
+    public Club() {
+    }
+
+    public Club(Long id, String title, String photoURL, String content, LocalDateTime createdOn, LocalDateTime updatedOn) {
+        this.id = id;
+        this.title = title;
+        this.photoURL = photoURL;
+        this.content = content;
+        this.createdOn = createdOn;
+        this.updatedOn = updatedOn;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    // toString() method
+    @Override
+    public String toString() {
+        return "Club{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", photoURL='" + photoURL + '\'' +
+                ", content='" + content + '\'' +
+                ", createdOn=" + createdOn +
+                ", updatedOn=" + updatedOn +
+                '}';
+    }
+}
