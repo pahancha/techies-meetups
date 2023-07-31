9122af385b15a6d4c092cb198291a3456033c27d
pahancha <pahanchathuranga123@gmail.com>
2023-07-31 23:16:10 +0530

    cors configure

--- src/main/java/com/techiesmeetups/web/WebApplication.java
+++ src/main/java/com/techiesmeetups/web/WebApplication.java
@@ -2,12 +2,27 @@ package com.techiesmeetups.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**") // Replace "/api/**" with your API endpoints path
+						.allowedOrigins("http://localhost:3000") // Replace with your React application's URL
+						.allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow specific HTTP methods
+			}
+		};
 	}
 
 }
