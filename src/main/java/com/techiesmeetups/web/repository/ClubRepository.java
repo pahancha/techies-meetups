5604e7afd3a212d1b970cb595a5c58e7011941b3
pahancha <pahanchathuranga123@gmail.com>
2023-08-06 17:44:24 +0530

    fix: jpql query to search both lower and upper cases.

--- src/main/java/com/techiesmeetups/web/repository/ClubRepository.java
+++ src/main/java/com/techiesmeetups/web/repository/ClubRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface ClubRepository extends JpaRepository<Club, Long> {
     Optional<Club> findByTitle(String url);
 
-     @Query("SELECT c from Club c WHERE c.title LIKE CONCAT('%', :query, '%')")
+    @Query("SELECT c FROM Club c WHERE LOWER(c.title) LIKE LOWER(CONCAT('%', :query, '%')) OR c.title LIKE CONCAT('%', :query, '%')")
     List<Club> searchClubs(String query);
 
 }
